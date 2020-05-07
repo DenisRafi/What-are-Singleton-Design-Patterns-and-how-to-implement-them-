dd5556d7bc31d9a25d9464636f2c625723835716
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-05-07 03:32:57 +0100

    Add files via upload

---
+++ What are Singleton Design Patterns and how to implement them in C#.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // There are various ways to implement the Singleton Pattern in C#. 
+            // The following are the common characteristics of a Singleton Pattern.
+
+            Console.WriteLine("What are Singleton Design Patterns and" +
+             " how to implement them in C# ? -By Denis Rafi");
+            Console.WriteLine();
+            Calculate.Instance.ValueOne = 10.5;
+            Calculate.Instance.ValueTwo = 5.5;
+            Console.WriteLine("Addition : " + Calculate.Instance.Addition());
+            Console.WriteLine("Subtraction : " + Calculate.Instance.Subtraction());
+            Console.WriteLine("Multiplication : " + Calculate.Instance.Multiplication());
+            Console.WriteLine("Division : " + Calculate.Instance.Division());
+            Console.WriteLine();
+            Calculate.Instance.ValueTwo = 10.5;
+            Console.WriteLine("Addition : " + Calculate.Instance.Addition());
+            Console.WriteLine("Subtraction : " + Calculate.Instance.Subtraction());
+            Console.WriteLine("Multiplication : " + Calculate.Instance.Multiplication());
+            Console.WriteLine("Division : " + Calculate.Instance.Division());
+            Console.ReadLine();
+        }
+    }
+    public sealed class Calculate
+    {
+        private Calculate() { }
+        private static Calculate instance = null;
+        public static Calculate Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Calculate();
+                }
+                return instance;
+            }
+        }
+        public double ValueOne
+        {
+            get;
+            set;
+        }
+        public double ValueTwo
+        {
+            get;
+            set;
+        }
+        public double Addition()
+        {
+            return ValueOne + ValueTwo;
+        }
+        public double Subtraction()
+        {
+            return ValueOne - ValueTwo;
+        }
+        public double Multiplication()
+        {
+            return ValueOne * ValueTwo;
+        }
+        public double Division()
+        {
+            return ValueOne / ValueTwo;
+        }
+    }
+}
+    
